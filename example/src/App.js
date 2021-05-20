b83785290d77566423256e5ac1cc622f0c5ab9b2
Asbjørn Enge <asbjorn@hanafjedle.net>
2021-05-20 13:03:41 +0200

    Working component

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,41 @@
-import React from 'react'
-
-import { ExampleComponent } from 'tezos-wallet-component-menu-tezid'
+import React, { useState } from 'react'
+import { TezosWallet } from 'tezos-wallet-component'
+import { TezosWalletMenuTezID } from 'tezos-wallet-component-menu-tezid'
+import 'tezos-wallet-component/dist/index.css'
 import 'tezos-wallet-component-menu-tezid/dist/index.css'
+import './App.css'
+
+const mockWallet = {
+  address: 'tz1iAAJhH465Cf3BnsKQ744XHypQGY1v7Ps9',
+  balance: 125
+}
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [showMenu, setShowMenu] = useState(false)
+  const [wallet, setWallet] = useState(null)
+
+  const handleConnect = () => {
+    setWallet(mockWallet)
+  }
+  const handleDisconnect = () => {
+    setWallet(null)
+    setShowMenu(false)
+  }
+
+  return (
+    <div className="App">
+      <TezosWallet
+        address={wallet?.address}
+        balance={wallet?.balance}
+        onConnectWallet={handleConnect}
+        onDisconnectWallet={handleDisconnect}
+        showMenu={showMenu}
+        onToggleMenu={() => setShowMenu(!showMenu)}
+      >
+        <TezosWalletMenuTezID address={wallet?.address} network="mainnet" />
+      </TezosWallet>
+    </div>
+  ) 
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState, useEffect } from 'react'
+import { TezIDProof } from '@tezid/proofs-component'
+import '@tezid/proofs-component/dist/index.css'
+import './index.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const TezosWalletMenuTezID = (props) => {
+  const [proofs, setProofs] = useState([])
+ 
+  useEffect(() => {
+    if (!props.address || !props.network) return
+    const fetchProofs = async () => {
+      try {
+        const res = await fetch(`https://tezid.net/api/${props.network}/getproofs/${props.address}`)
+        const _proofs = await res.json()
+        setProofs(_proofs.filter(p => p.verified))
+      } catch(e) {
+        console.error(e)
+      }
+    }
+    fetchProofs()
+  }, [props.address, props.network, setProofs])
+
+  if (!props.address) return null
+ 
+  const proofItems = proofs.map(p => 
+    <TezIDProof 
+      key={p.id} 
+      proof={p} 
+    />
+  )
+
+  return (
+    <div className="TezosWalletMenuTezID" onClick={props?.onClick}>
+      { proofs.length > 0 &&
+        <React.Fragment>
+        <div className="TezosWalletMenuTezIDTop">You registered <a target="_blank" rel="noreferrer" href="https://tezid.net">TezID</a> proofs:</div>
+        <div className="TezosWalletMenuTezIDProofs">
+          {proofItems}
+        </div>
+        </React.Fragment>
+      }
+      { proofs.length === 0 &&
+        <div className="TezosWalletMenuTezIDNoProofs">
+          <div className="label">
+            You have no registered TezID proofs. Register <a target="_blank" rel="noreferrer" href="https://tezid.net">here</a>.
+          </div>
+          <div className="icon">
+            <img src="https://tezid.net/logo-notext.svg" alt="tezidlogo" />
+          </div>
+        </div>
+      }
+    </div>
+  )
 }
